f7a6952297958fe43f911da332342fa3f7eb268c
gslaczkowski <gabriel@codigoeconteudo.com.br>
2022-10-29 12:40:27 -0300

    Cria a página inicial, adiciona os estilos e o core.js (que fará a requisição)

---
+++ scripts/core.js
@@ -0,0 +1,39 @@
+(function () {
+    let httpRequest;
+    document.getElementById("consulta").onclick = function () {
+        makeRequest('https://minhareceita.org/' + document.getElementById('cnpj').value);
+    };
+
+    function makeRequest(url) {
+        if (window.XMLHttpRequest) {
+            httpRequest = new XMLHttpRequest();
+        } else if (window.ActiveXObject) { // IE
+            try {
+                httpRequest = new ActiveXObject("Msxml2.XMLHTTP");
+            } catch (e) {
+                try {
+                    httpRequest = new ActiveXObject("Microsoft.XMLHTTP");
+                } catch (e) {
+                }
+            }
+        }
+
+        if (!httpRequest) {
+            alert('Giving up :( Cannot create an XMLHTTP instance');
+            return false;
+        }
+        httpRequest.onreadystatechange = alertContents;
+        httpRequest.open('GET', url);
+        httpRequest.send();
+    }
+
+    function alertContents() {
+        if (httpRequest.readyState === 4) {
+            if (httpRequest.status === 200) {
+                alert(httpRequest.responseText);
+            } else {
+                alert('There was a problem with the request.');
+            }
+        }
+    }
+})();
