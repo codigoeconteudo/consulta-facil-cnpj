55b9ba1ed548cd3bdfce575b6ba4374d203577de
gslaczkowski <gabriel@codigoeconteudo.com.br>
2022-10-29 15:18:16 -0300

    Inclusão de Mais Campos no Resultado

--- scripts/core.js
+++ scripts/core.js
@@ -35,10 +35,21 @@
 
                 const response = JSON.parse(httpRequest.responseText);
 
-                document.getElementById("cnpj").innerText = response.cnpj;
-                document.getElementById("razao_social").innerText = response.razao_social;
+                document.getElementById("cnpj").innerHTML = response.cnpj;
                 document.getElementById("nome_fantasia").innerText = response.nome_fantasia;
-
+                document.getElementById("razao_social").innerText = response.razao_social;
+                document.getElementById("logradouro").innerText = response.logradouro;
+                document.getElementById("numero").innerText = response.numero;
+                document.getElementById("complemento").innerText = response.complemento;
+                document.getElementById("bairro").innerText = response.bairro;
+                document.getElementById("municipio").innerText = response.municipio;
+                document.getElementById("uf").innerText = response.uf;
+                document.getElementById("cep").innerText = response.cep;
+                document.getElementById("ddd_telefone_1").innerText = response.ddd_telefone_1;
+                document.getElementById("ddd_telefone_2").innerText = response.ddd_telefone_2;
+                document.getElementById("email").innerText = response.email;
+                document.getElementById("porte").innerText = response.porte;
+                document.getElementById("natureza_juridica").innerText = response.natureza_juridica;
 
 
             } else {
